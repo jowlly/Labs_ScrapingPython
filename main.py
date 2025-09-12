17fb630c59b01ca9bbbb5fa682cd28a757ff7c21
Jowlly <98984146+jowlly@users.noreply.github.com>
2025-09-12 17:12:10 +0300

    data set for CAD

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pandas as pd
 import requests
 
 url = 'https://www.cbr-xml-daily.ru/archive/'
+val ='CAD'
 days = 32
 months = 13
-years = 31
+years = 2
 result=[]
 for y in range(years):
     for m in range(1,months):
         for d in range(1,days):
-            cur_date = f"{1995+y}/{m if m>9 else "0"+str(m)}/{d if d>9 else "0"+str(d)}"
+            cur_date = f"{2024+y}/{m if m>9 else "0"+str(m)}/{d if d>9 else "0"+str(d)}"
             cur_url = url +cur_date +"/daily_json.js"
 
             print("Скрапинг страницы: ", cur_url)
@@ -19,8 +20,8 @@ for y in range(years):
                 print("Не найдено")
 
                 continue
-            print(json_response["Valute"]["USD"])
-            result.append([str(json_response["Date"]).split("T")[0],json_response["Valute"]["USD"]["Value"]])
+            print(json_response["Valute"][val])
+            result.append([str(json_response["Date"]).split("T")[0],json_response["Valute"][val]["Value"]])
 
 df = pd.DataFrame(result, columns = ["date", "value"])
 
